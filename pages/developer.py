549323aefcc93870baae2495d088469c1bca9536
Jose Espinoza <espinozajgx@gmail.com>
2025-12-14 20:56:50 +0100

    test actions

--- pages/developer.py
+++ pages/developer.py
@@ -40,13 +40,11 @@ def hash_password(password: str) -> str:
     hashed = bcrypt.hashpw(password, bcrypt.gensalt())
     return hashed.decode("utf-8")
 
-
 def verify_password(password: str, hashed: str) -> bool:
     """
     Verifica si un password coincide con su hash.
     """
     return bcrypt.checkpw(password.encode("utf-8"), hashed.encode("utf-8"))
-
 
 # ============================
 #   UI DEL GESTOR
